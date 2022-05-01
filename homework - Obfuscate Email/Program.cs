5ab33ccfff11c2148e035f768699b25e3f3e9a83
catalinacris <catalinacucos14@yahoo.com>
2022-05-01 05:48:52 +0200

    Add project files.

---
+++ homework - Obfuscate Email/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework___Obfuscate_Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your email adress: ");
+            string email = Console.ReadLine();
+
+            int atPosition = email.IndexOf('@');
+            if (atPosition > -1)
+            {
+                string newEmail = new string('*', atPosition) + email.Substring(atPosition);
+                Console.WriteLine("Your email adress is " + newEmail);
+            }
+
+        }
+    }
+}
